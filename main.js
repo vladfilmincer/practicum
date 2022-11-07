00fd730e5bc645fa1ed9dc6d5375c100932c26d6
vladfilmincer <vladfilmincer@gmail.com>
2022-11-06 21:38:04 -0600

    mainjs 1st

--- main.js
+++ main.js
@@ -3,3 +3,69 @@
 
 
 
+ let user = {
+  name: 'John',
+  surname: 'Smith',
+ }
+
+user['name']='pete'
+console.log(user.name);
+
+
+
+
+
+
+
+
+function showVerticalMessage(word){
+  i = 0
+  for(let char of word){
+    if (word[0] == "s" && char == 's'){
+      console.log(char.toUpperCase() + '\n');
+    } else {
+      console.log(char + '\n');
+    }
+    
+    if (i >= 6){
+      break;
+    }
+
+    i++
+  }
+}
+
+//showVerticalMessage('strada')
+
+/*
+function buildFun(n){
+
+	var res = []
+
+	for (let i = 0; i< n; i++){
+		res.push(function(){
+			console.log(i)
+		})
+	}
+	return res
+}
+*/
+
+
+function buildFun(n){
+
+	var res = []
+
+	for (var i = 0; i< n; i++){
+		res.push((function(i){
+			return function(){
+        return i
+      }
+		})(i))
+	}
+	return res
+}
+
+
+
+console.log(buildFun(5)[2]())
